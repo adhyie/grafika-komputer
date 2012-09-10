32e77c29dcf0c493dc4cfdbdfd333f38471bc77e
unknown <eadams@vaio.(none)>
2012-09-10 11:39:16 +0700

    upload source code demo pertemuan ke-2

---
+++ your_projects/Pertemuan_02/HelloOpenGLExample.cpp
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <GL/glfw.h>
+#include "GLFWApplication.h"
+
+
+class HelloOpenGLExample: public GLFWApplication
+{
+
+	virtual void init(){
+	
+	}
+
+	virtual void update(double time){
+	
+	}
+
+	virtual void render(){
+		    // Setup viewport
+		    glViewport( 0, 0, width, height );
+
+			// Clear color buffer to black
+			glClearColor( 0.0f, 0.0f, 0.0f, 0.0f );
+			glClear( GL_COLOR_BUFFER_BIT );
+
+			// Select and setup the projection matrix
+			glMatrixMode( GL_PROJECTION );
+			glLoadIdentity();
+			gluPerspective( 65.0f, (GLfloat)width/(GLfloat)height, 1.0f, 100.0f );
+
+			// Select and setup the modelview matrix
+			glMatrixMode( GL_MODELVIEW );
+			glLoadIdentity();
+			gluLookAt( 0.0f, 1.0f, 0.0f,    // Eye-position
+					   0.0f, 20.0f, 0.0f,   // View-point
+					   0.0f, 0.0f, 1.0f );  // Up-vector
+
+			// Draw colorful triangle with TRIANGLES primitive mode
+			glTranslatef( 0.0f, 14.0f, 0.0f );
+		
+			glBegin( GL_TRIANGLES );
+			  glColor3f( 1.0f, 0.0f, 0.0f );
+			  glVertex3f( -5.0f, 0.0f, -4.0f );
+			  glColor3f( 0.0f, 1.0f, 0.0f );
+			  glVertex3f( 5.0f, 0.0f, -4.0f );
+			  glColor3f( 0.0f, 0.0f, 1.0f );
+			  glVertex3f( 0.0f, 0.0f, 6.0f );
+			glEnd();
+	}
+
+};
+
+int main(void){
+		HelloOpenGLExample app;
+		app.start(800, 600, true, "Hello OpenGL!", true);
+}
---
+++ your_projects/ProjectTemplate/GLFWApplication.h
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <GL/glfw.h>
+
+
+class GLFWApplication
+{
+public:
+	virtual void init() = 0;
+	virtual void update(double time) = 0;
+	virtual void render() = 0;
+	int width, height, x;
+    double t;
+	void start(int width, int height, bool fullscreen, const char * title, bool vsync){
+		// Initialise GLFW
+		if( !glfwInit() )
+		{
+			fprintf( stderr, "Failed to initialize GLFW\n" );
+			exit( EXIT_FAILURE );
+		}
+
+		// Open a window and create its OpenGL context
+		if( !glfwOpenWindow( width, height, 0,0,0,0, 0,0, fullscreen?GLFW_FULLSCREEN:GLFW_WINDOW ) )
+		{
+			fprintf( stderr, "Failed to open GLFW window\n" );
+
+			glfwTerminate();
+			exit( EXIT_FAILURE );
+		}
+
+		// Set a window title
+		glfwSetWindowTitle(title);
+
+		// Ensure we can capture the escape key being pressed below
+		glfwEnable( GLFW_STICKY_KEYS );
+
+		// Enable vertical sync (on cards that support it)
+		glfwSwapInterval( vsync?1:0 );
+
+
+
+		do
+		{
+			t = glfwGetTime();
+			glfwGetMousePos( &x, NULL );
+
+			// Get window size (may be different than the requested size)
+			glfwGetWindowSize( &width, &height );
+
+			// Special case: avoid division by zero below
+			height = height > 0 ? height : 1;
+			
+			// Your update application logics procedure here ...
+			update(t);
+			
+			// Your rendering procedure here ...
+			render();
+
+			// Swap buffers
+			glfwSwapBuffers();
+
+		} // Check if the ESC key was pressed or the window was closed
+		while( glfwGetKey( GLFW_KEY_ESC ) != GLFW_PRESS &&
+			   glfwGetWindowParam( GLFW_OPENED ) );
+
+		// Close OpenGL window and terminate GLFW
+		glfwTerminate();
+
+		exit( EXIT_SUCCESS );
+}
+
+
+
+
+	
+
+};
